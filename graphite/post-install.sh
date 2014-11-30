b7d1174a33667abdd95fb6d38fdfa6ca7912d3b5
Olivier JAN <olivierjan.com@gmail.com>
2014-11-30 11:19:31 +0100

    update: various

--- graphite/post-install.sh
+++ graphite/post-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-GRAFANA_VERSION="1.5.4"
+GRAFANA_VERSION="1.8.1"
 COLLECTD_VERSION="5.4.1"
 
 apt_install () {
@@ -29,10 +29,10 @@ packages_install () {
 	sudo mv grafana-${GRAFANA_VERSION}/src /usr/share/nginx/html/grafana
 	# be sure it can execute start.sh to start circus and carbon-cache at restart
 	sudo chmod 755 ~/files/scripts/start.sh
-	sudo dpkg -i collectd-${COLLECTD_VERSION}_amd64.deb
+	#sudo dpkg -i collectd-${COLLECTD_VERSION}_amd64.deb
 	sudo apt-get --yes --fix-broken install
 
-	sudo update-rc.d collectd defaults
+	#sudo update-rc.d collectd defaults
 
 }
 
@@ -40,7 +40,7 @@ configuration_install () {
 	cd ~/files/etc/
 	sudo cp nginx/sites-available/default /etc/nginx/sites-available/
 	sudo cp grafana/config.js /usr/share/nginx/html/grafana/
-	sudo cp -r collectd/* /opt/collectd/etc/
+	#sudo cp -r collectd/* /opt/collectd/etc/
 	sudo cp rc.local /etc/rc.local
 
 }
